f6dd9c7d431edda26ce0c9d69581ca2e764b6082
Kapex-Coder <kapex.coder@gmail.com>
2024-08-19 00:43:05 +0530

    Updated: AddWidgetForm UI

--- src/components/Forms/AddWidgetForm.jsx
+++ src/components/Forms/AddWidgetForm.jsx
@@ -6,6 +6,8 @@ import {
   updateChartField,
 } from "../../redux/slices/widgetFormSlice";
 import { addWidget } from "../../redux/slices/categoriesSlice";
+import { styled } from "@mui/material/styles";
+
 import {
   Box,
   Button,
@@ -15,7 +17,11 @@ import {
   Typography,
 } from "@mui/material";
 
-function AddWidgetForm({categoryId, handleClose}) {
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  "& input": { width: "100%", fontSize: "0.875rem", padding: "0.3rem 0.5rem" },
+}));
+
+function AddWidgetForm({ categoryId, handleClose }) {
   const widgetFormData = useSelector((state) => state.widgetForm.value);
   const dispatch = useDispatch();
 
@@ -29,22 +35,27 @@ function AddWidgetForm({categoryId, handleClose}) {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    dispatch(addWidget({categoryId, widget: widgetFormData }));
+    dispatch(addWidget({ categoryId, widget: widgetFormData }));
     handleClose();
-  }
+  };
 
   return (
     <Box
       component="form"
-      sx={{ padding: "1rem", flexGrow: 1 }}
+      sx={{
+        padding: "1rem",
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+      }}
       onSubmit={handleFormSubmit}
       noValidate
       autoComplete="off">
-      <Box>
+      <Box sx={{marginBottom: "0.6rem"}}>
         <InputLabel htmlFor="name">Name:</InputLabel>
-        <TextField
+        <StyledTextField
           id="name"
-          sx={{ width: "100%" }}
+          sx={{width: "100%"}}
           size="small"
           placeholder="Enter the widget name..."
           variant="outlined"
@@ -52,6 +63,7 @@ function AddWidgetForm({categoryId, handleClose}) {
           onChange={(e) =>
             handleInputChange({ key: "name", value: e.target.value })
           }
+          required
         />
       </Box>
       <Box>
@@ -61,32 +73,52 @@ function AddWidgetForm({categoryId, handleClose}) {
             <Stack
               key={field.id}
               direction="row"
+              sx={{marginBottom: "0.6rem"}}
               gap={1}>
-              <TextField
-                sx={{ width: "100%" }}
+              <StyledTextField
                 size="small"
                 placeholder="Enter label..."
                 variant="outlined"
-                onChange={(e) => handleChartFieldInputChange({chartFieldId: field.id, key: "label", value: e.target.value })}
+                onChange={(e) =>
+                  handleChartFieldInputChange({
+                    chartFieldId: field.id,
+                    key: "label",
+                    value: e.target.value,
+                  })
+                }
               />
-              <TextField
-                sx={{ width: "100%" }}
+              <StyledTextField
                 size="small"
+                sx={{ width: "8rem" }}
                 placeholder="Enter value..."
                 variant="outlined"
-                onChange={(e) => handleChartFieldInputChange({chartFieldId: field.id, key: "value", value: parseInt(e.target.value)})}
+                onChange={(e) =>
+                  handleChartFieldInputChange({
+                    chartFieldId: field.id,
+                    key: "value",
+                    value: parseInt(e.target.value),
+                  })
+                }
               />
-              <TextField
+              <StyledTextField
                 type="color"
-                sx={{ width: "6rem" }}
+                sx={{ width: "2rem" }}
                 size="small"
                 variant="outlined"
-                onChange={(e) => handleChartFieldInputChange({chartFieldId: field.id, key: "color", value: e.target.value})}
+                onChange={(e) =>
+                  handleChartFieldInputChange({
+                    chartFieldId: field.id,
+                    key: "color",
+                    value: e.target.value,
+                  })
+                }
               />
             </Stack>
           ))}
         <Button
           variant="contained"
+          size="small"
+          sx={{ marginLeft: "auto", display: "block" }}
           onClick={() => dispatch(addChartFields())}>
           Add Chart Fields
         </Button>
@@ -97,11 +129,15 @@ function AddWidgetForm({categoryId, handleClose}) {
         justifyContent="end"
         sx={{ marginTop: "auto" }}>
         <Button
-          variant="contained"
+          variant="outlined"
           onClick={handleClose}>
           Cancel
         </Button>
-        <Button type="submit" variant="contained">Add Widget</Button>
+        <Button
+          type="submit"
+          variant="contained">
+          Add Widget
+        </Button>
       </Stack>
     </Box>
   );

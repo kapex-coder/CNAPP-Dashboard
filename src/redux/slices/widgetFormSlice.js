0d22e4c4b3e3f3a64cf672de17a21be3a8738bb9
Kapex-Coder <kapex.coder@gmail.com>
2024-08-19 14:38:06 +0530

    Fixed: RandomId issue of widgets

--- src/redux/slices/widgetFormSlice.js
+++ src/redux/slices/widgetFormSlice.js
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     value: {
-        id: crypto.randomUUID(),
         isActive: true,
         name: "",
         chartData: []

a16360ac8f346d5a328389f44c496b7fb01bc654
Kapex-Coder <kapex.coder@gmail.com>
2024-08-19 22:47:23 +0530

    Added: Search feature

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import DashboardCategory from "./DashboardCategory";
 import DashboardOptions from "./DashboardOptions";
@@ -5,14 +6,36 @@ import { Container } from "@mui/material";
 
 export default function Dashboard() {
   const categories = useSelector((state) => state.categories.value);
+  const searchValue = useSelector((state) => state.search.value);
+  const [filteredArray, setFilteredArray] = useState([]);
+
+  const getFilteredArray = () => {
+    if (!searchValue) {
+      setFilteredArray(categories);
+      return;
+    }
+
+    const filteredArray = categories.map((cat) => ({
+      ...cat,
+      widgets: cat.widgets.filter((widget) =>
+        widget.name.toLowerCase().includes(searchValue.toLowerCase())
+      ),
+    }));
+
+    setFilteredArray(filteredArray);
+  };
+
+  useEffect(() => {
+    getFilteredArray();
+  }, [searchValue, categories]);
 
   return (
     <Container
       maxWidth="xl"
       sx={{ padding: "1rem 0" }}>
       <DashboardOptions />
-      {categories &&
-        categories.map((category) => (
+      {filteredArray &&
+        filteredArray.map((category) => (
           <DashboardCategory
             category={category}
             key={category.id}
